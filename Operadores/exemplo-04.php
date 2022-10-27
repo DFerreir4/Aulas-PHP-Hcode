377dbbb0bc8ecbafc2d26ca87f43b3b31873ea7e
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-27 19:25:43 -0300

    Operadores de comparação

---
+++ Operadores/exemplo-04.php
@@ -0,0 +1,29 @@
+<?php
+
+// operadores de comparação
+
+$a = 55.0;
+$b = 55;
+
+var_dump($a > $b);
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+var_dump($a === $b); // 1 = só é atribuição - 2 == é de comparação - 3 === é comparação de tipo
+//se o tipo for importante para o que você ta precisando então usa 3 =s
+echo "<br>";
+
+var_dump($a !== $b); // a ! é "não" ou diferente... a mesma lógica se aplica aqui
+//foi comparado a tipagem adicinando 2 iguais... mas normalmente é utilizado só !=
+
+echo "<br>";
+
+
+
+
+
+?>
