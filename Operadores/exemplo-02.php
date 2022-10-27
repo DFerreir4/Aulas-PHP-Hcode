f7be76c0a49bfb151931c4ef03be1156bc7c47bc
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-27 18:28:31 -0300

    Operador de atribuição - Manipulando valores

---
+++ Operadores/exemplo-02.php
@@ -0,0 +1,22 @@
+<?php
+//operador de atribuição
+$valorTotal = 0;
+
+$valorTotal += 100;
+$valorTotal += 25;
+//+= pega o valor atual da váriavel e soma com o valor recebido
+//total que foi impresso no echo até aqui foi 125
+
+$valorTotal -= 10;
+// a mesma lógica no desconto acima que imprime 115
+
+$valorTotal *= .9;
+//acima ele vai dar 10% de desconto
+//o valor total ficou 103.5
+
+
+
+echo $valorTotal;
+
+
+?>
