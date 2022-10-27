ef82f5e46172f6917a62371dab45daccc81d52fd
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-27 18:39:08 -0300

    Operadores aritiméticos

---
+++ Operadores/exemplo-03.php
@@ -0,0 +1,22 @@
+<?php
+
+//Operadores aritiméticos
+
+$a = 10;
+$b = 2;
+
+echo $a + $b."<br>"; //soma
+
+echo $a - $b."<br>"; // subtração
+
+echo $a * $b."<br>"; // multiplicação
+
+echo $a / $b."<br>"; // divisão
+
+echo $a % $b."<br>"; // Módulo
+
+echo $a ** $b."<br>"; // exponenciação
+
+
+
+?>
