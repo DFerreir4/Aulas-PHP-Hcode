5efe15eff523ca915004bf22bd29289022f9a209
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-27 17:55:05 -0300

    Operadores de atribuição

---
+++ Operadores/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+
+//Operadores de atribuição
+
+$nome = "hcode"; //operador de atribuição
+
+echo $nome. " mais alguma coisa <br>"; //operador de string e concatenação
+
+
+$nome .= " treinamentos"; // o .= acrescente valores a variavel
+
+echo $nome;
+
+
+
+
+
+
+?>
