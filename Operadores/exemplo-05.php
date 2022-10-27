11d2a52f8b3811b79c5410e6333cc043feada5ae
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-27 19:28:36 -0300

    Operadores de spacechip

---
+++ Operadores/exemplo-05.php
@@ -0,0 +1,19 @@
+<?php
+//Operador spacechip
+
+
+$a = 50;
+$b = 35;
+
+var_dump($a <=> $b);
+
+//o que foi impresso foi int(1)
+// esse operador mostra 1 se o $a for maior
+// se os dois operadores forem iguais o resultado imprime 0
+// se o $b for maior, -1
+
+
+
+
+
+?>
