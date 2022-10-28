b2464a65943e3c9c4d6943e3cbcac0543f369a2d
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-28 15:50:01 -0300

    Interpolação de variáveis

---
+++ Strings/exemplo-01.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = "Hcode";
+
+$nome2 = "Treinamentos";
+
+//var_dump($nome,$nome2);
+
+echo "ABC $nome"; // imterpolação de variáveis.
+// as aspas duplas verifica se tem alguma variavel no texto.
+// as aspas simples transforma tudo dentro em texto.
+
+
+
+
+
+
+
+
+?>
