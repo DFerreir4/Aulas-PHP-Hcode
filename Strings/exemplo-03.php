0b0c857fe31a97caf7f74ca23d09760b45225328
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-28 17:13:41 -0300

    função que troca letras de uma determinada palavra ou texto.

---
+++ Strings/exemplo-03.php
@@ -0,0 +1,16 @@
+<?php
+
+$empresa = "hcode";
+
+$empresa = str_replace("o",0,$empresa);
+$empresa = str_replace("e",3,$empresa);
+
+//função que troca letras de uma determinada palavra ou texto.
+
+
+
+echo $empresa;
+
+
+
+?>
