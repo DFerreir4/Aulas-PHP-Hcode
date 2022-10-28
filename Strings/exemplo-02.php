36526b3d1b9adbbdcf18d8f802b583f455b3484f
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-28 16:42:37 -0300

    Funções nativas do php - Strings

---
+++ Strings/exemplo-02.php
@@ -0,0 +1,26 @@
+<?php
+
+$nome = "diego ferreira";
+
+echo strtoupper($nome);
+//função interna do php  que transforma string em maiusculos
+
+echo "<br>";
+
+echo strtolower($nome);
+//função interna do php que transforma string em minusculas
+
+echo "<br>";
+
+echo ucfirst($nome);
+//função interna do php que transforma apenas a primeira letra maiuscula
+
+echo "<br>";
+
+echo ucwords($nome);
+//função interna do php que transforma as primeiras letras maiusculas de cada palavra.
+echo "<br>";
+
+
+
+?>
