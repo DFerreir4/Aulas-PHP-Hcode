d7ca2f9b88dab7893bdff9b856753523b27e03fa
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-10-27 21:05:07 -0300

    Operadores de incremento e decremento

---
+++ Operadores/exemplo-07.php
@@ -0,0 +1,25 @@
+<?php
+//operadores de incremento e decremento
+
+$a = 10;
+//incremento
+echo ++$a;
+echo "<br>";
+echo $a;
+echo "<br>";
+echo $a = $a + 1;
+
+echo "<br>";
+
+//decremento
+
+echo --$a;
+
+
+
+
+
+
+
+
+?>
