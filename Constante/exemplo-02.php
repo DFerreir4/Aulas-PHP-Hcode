fe8483d4c64f847abcdc2c13db80bff2cb6a0f17
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-11-05 23:41:28 -0300

    Constante conexão base de dados

---
+++ Constante/exemplo-02.php
@@ -0,0 +1,21 @@
+<?php
+
+define("BANCO_DE_DADOS",
+
+[
+    
+"127.0.0.1",
+"root",
+"password",
+"test"
+
+]//,true
+// se quiser que seja case insensitive, colocar true para não obrigar escrever maiusculo ou minusculo
+
+);
+
+print_r(BANCO_DE_DADOS);
+
+
+
+?>
