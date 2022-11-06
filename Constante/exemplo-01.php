d8c7609edbb3f0c014b6cfe57f913b7f081a8499
Diego Ferreira <d.ferreira.andr@gmail.com>
2022-11-05 23:35:33 -0300

    Constante

---
+++ Constante/exemplo-01.php
@@ -0,0 +1,13 @@
+<?php
+
+define("SERVIDOR","127.0.0.1");
+
+//precisa passar dois parametros para essa função
+//o primeiro é o nome da constante
+//o segundo é o valor da constante
+
+echo SERVIDOR;
+
+//foi exibido na página o valor do ip: 127.0.0.1
+
+?>
